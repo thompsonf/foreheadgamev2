e7b7343590446ed9cfd20a6b9902921c720d8722
Frank Thompson <frankthompson@gmail.com>
2025-01-02 14:12:51 -0800

    fix page title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { ColorSchemeScript, mantineHtmlProps, MantineProvider } from '@mantine/c
 import { theme } from '../theme';
 
 export const metadata = {
-  title: 'Mantine Next.js template',
-  description: 'I am using Mantine with Next.js!',
+  title: 'Forehead Game',
+  description: 'Play the Forehead Game online',
 };
 
 export default function RootLayout({ children }: { children: any }) {
